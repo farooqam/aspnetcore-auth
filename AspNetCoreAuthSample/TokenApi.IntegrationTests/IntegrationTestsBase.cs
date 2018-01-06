a0aff36c5679066a8650dcf0daa2bfd0f7f352b0
Farooq Mahmud <farooq_am@hotmail.com>
2018-01-05 22:42:06 -0800

    Integration tests.

---
+++ AspNetCoreAuthSample/TokenApi.IntegrationTests/IntegrationTestsBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
+using Newtonsoft.Json;
+using TokenApi.Security.Common;
+
+namespace TokenApi.IntegrationTests
+{
+    public abstract class IntegrationTestsBase
+    {
+        protected Mock<ICredentialValidator> MockCredentialValidator { get; set; }
+        public HttpClient HttpClient { get; }
+
+        protected IntegrationTestsBase()
+        {
+            MockCredentialValidator = new Mock<ICredentialValidator>();
+
+            var server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>()
+                .ConfigureServices(
+                    services => { services.TryAddTransient(provider => MockCredentialValidator.Object); }));
+
+            HttpClient = server.CreateClient();
+        }
+
+        protected StringContent Stringify<T>(T obj) where T : class
+        {
+            return new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+        }
+    }
+}
---
+++ AspNetCoreAuthSample/TokenApi.IntegrationTests/TokenControllerIntegrationTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Newtonsoft.Json;
+using TokenApi.Common;
+using Xunit;
+
+namespace TokenApi.IntegrationTests
+{
+    public class TokenControllerIntegrationTests : IntegrationTestsBase
+    {
+        [Fact]
+        public async Task Post_Returns_Ok_Status_And_Token()
+        {
+            // Arrange
+            MockCredentialValidator.Setup(m => m.ValidateAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(true);
+
+            var requestModel = new PostTokenRequestModel
+            {
+                Username = "farooq",
+                Audience = "integration test",
+                Issuer = "integration test library",
+                Password = "hello!"
+            };
+
+            // Act
+            var response = await HttpClient.PostAsync("api/token", Stringify(requestModel));
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var content = await response.Content.ReadAsStringAsync();
+            var responseModel = JsonConvert.DeserializeObject<PostTokenResponseModel>(content);
+            responseModel.Token.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public async Task Post_WhenCredentialsNotValid_Returns_BadRequest_Status()
+        {
+            // Arrange
+            MockCredentialValidator.Setup(m => m.ValidateAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(false);
+
+            var requestModel = new PostTokenRequestModel
+            {
+                Username = "farooq",
+                Audience = "integration test",
+                Issuer = "integration test library",
+                Password = "hello!"
+            };
+
+            // Act
+            var response = await HttpClient.PostAsync("api/token", Stringify(requestModel));
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
