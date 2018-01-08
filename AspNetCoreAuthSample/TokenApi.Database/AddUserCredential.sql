5a3bb57eee19797114a4ed92d5170f7fa8ce7507
Farooq Mahmud <farooq_am@hotmail.com>
2018-01-08 12:48:42 -0800

    Sproc to add user credential.

---
+++ AspNetCoreAuthSample/TokenApi.Database/AddUserCredential.sql
@@ -0,0 +1,37 @@
+﻿CREATE PROCEDURE [dbo].[AddUserCredential]
+	@userId UNIQUEIDENTIFIER,
+	@password NVARCHAR(256),
+	@executedByUserId UNIQUEIDENTIFIER
+AS
+	DECLARE @eventName NVARCHAR(256);
+	SET @eventName = 'Credentials-Add [' +  CONVERT(NVARCHAR(36), @userId) + ']';
+
+	BEGIN TRY
+		BEGIN TRANSACTION
+			INSERT INTO [dbo].[UserCredentials] (
+				[UserId],
+				[Password])
+			VALUES (
+				@userId,
+				@password);
+
+			EXEC [dbo].[AuditEvent] 
+				@eventName = @eventName,
+				@eventDescription = 'A credential for a user was added.',
+				@executedByUserId = @executedByUserId;
+
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		IF (XACT_STATE()) <> 0
+		BEGIN	
+			ROLLBACK TRANSACTION;
+
+			EXEC [dbo].[AuditEvent] 
+				@eventName = @eventName,
+				@eventDescription = 'A credential for a user was attempted to be added but failed.',
+				@executedByUserId = @executedByUserId;
+			
+		END;
+	END CATCH;
+RETURN 0
--- AspNetCoreAuthSample/TokenApi.Database/UserCredentials.sql
+++ AspNetCoreAuthSample/TokenApi.Database/UserCredentials.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[UserCredentials]
 (
-	[UserId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	[UserId] UNIQUEIDENTIFIER NOT NULL ,
 	[Password] NVARCHAR(256) NOT NULL, 
-    CONSTRAINT [FK_UserCredentials_UserId_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id])
+    CONSTRAINT [FK_UserCredentials_UserId_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id]), 
+    PRIMARY KEY ([UserId], [Password])
 )
